799d3771c64e66117bf4e221a0f1a64998f12be5
shubhutf <shubhhu.raj@gmail.com>
2026-06-24 03:06:09 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,15 @@
+// import axios from "axios";
+
+// const api = axios.create({
+//   baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000",
+// });
+
+// export default api;
+
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000",
+  baseURL: "https://amazon-clone-backend-hxsi.onrender.com",
 });
 
 export default api;
